24d397509f98b04200a3bce7c1a19119e2d6e94a
Austin Smith <smith.austin149@gmail.com>
2018-11-23 20:05:19 -0800

    problem 8

---
+++ problem008/problem008.py
@@ -0,0 +1,23 @@
+
+def getProduct(values):
+    prod = 1
+    for value in values:
+        prod = value*prod
+    return prod
+
+file = open("input.txt", "r")
+
+lastValues = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+highProd = 1
+for line in file:
+    line.rstrip()
+    for number in line:
+        if number == "\n": continue
+        lastValues.pop(0)
+        lastValues.append(int(number))
+        currentProd = getProduct(lastValues)
+        if currentProd > highProd:
+            highProd = currentProd
+
+
+print(highProd)
